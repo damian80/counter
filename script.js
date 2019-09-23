139aee4e9779552cdf3115381cfe406ccd41eba5
Damian Ciasnocha <daneek80@gmail.com>
2019-09-23 05:59:09 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ let count= 0;
 
 addCount.addEventListener('click',increaseCounter);
 lowerCount.addEventListener('click', decreaseCounter);
+
+function increaseCounter(){
+    count++;
+    counter.innerHTML=count;
+if(counter.innerHTML>'0'){
+    counter.style.color='#4caf50';
+}
+else if (counter.innerHTML==='0'){
+    counter.style.color = 'white';
+}
+counter.animate([{opacity:'0.2'},{opacity:'1.0'}],
+{duration:500,fill:'forwards'});
+}
+function decreaseCounter() {
+    count--;
+    counter.innerHTML = count;
+    if (counter.innerHTML < '0') {
+        counter.style.color = 'red';
+    }
+    else if (counter.innerHTML === '0') {
+        counter.style.color = 'white';
+    }
+    counter.animate([{ opacity: '0.2' }, { opacity: '1.0' }],
+    { duration:500, fill: 'forwards' });
+}
